9bf8e7061a1a50cd70a17768117ab549f21bd10c
OdayMohamed <odaydieg@gmail.com>
2019-07-14 17:20:58 +0200

    last files

---
+++ Weather Station/View/ButtonDesign.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonDesign.swift
+//  Weather Station
+//
+//  Created by Oday Dieg on 6/28/19.
+//  Copyright © 2019 Oday Dieg. All rights reserved.
+//
+
+import UIKit
+
+class ButtonDesign: UIButton {
+
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        layer.cornerRadius = 12
+    }
+    
+    
+}
---
+++ Weather Station/View/ViewDesign.swift
@@ -0,0 +1,18 @@
+//
+//  ViewDesign.swift
+//  Weather Station
+//
+//  Created by Oday Dieg on 6/28/19.
+//  Copyright © 2019 Oday Dieg. All rights reserved.
+//
+
+import UIKit
+
+class ViewDesign: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 15
+        
+    }
+}
